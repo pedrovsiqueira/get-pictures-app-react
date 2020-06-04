062f864e30d523ddb681a1e095c4a737b4caed4e
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-04 09:52:59 -0300

    Add cart icon to filled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context";
 
 function Header() {
+  const { cartItems } = useContext(Context);
+
+  const cartClassName =
+    cartItems.length ? "ri-shopping-cart-fill" : "ri-shopping-cart-line";
+
   return (
     <header>
       <h2>
         <Link to="/">Pic Some</Link>
       </h2>
-
-      <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
-      </Link>
-      
+      <i className={`${cartClassName} ri-fw ri-2x`}></i>
     </header>
   );
 }
