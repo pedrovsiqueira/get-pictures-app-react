4b6f4195ae4cc25b3d3103dc2bd0c442f32cad59
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-04 10:28:17 -0300

    Start checkout page

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,18 @@ import { Context } from "../Context";
 function Header() {
   const { cartItems } = useContext(Context);
 
-  const cartClassName =
-    cartItems.length ? "ri-shopping-cart-fill" : "ri-shopping-cart-line";
+  const cartClassName = cartItems.length
+    ? "ri-shopping-cart-fill"
+    : "ri-shopping-cart-line";
 
   return (
     <header>
       <h2>
         <Link to="/">Pic Some</Link>
       </h2>
-      <i className={`${cartClassName} ri-fw ri-2x`}></i>
+      <Link to="/cart">
+        <i className={`${cartClassName} ri-fw ri-2x`}></i>
+      </Link>
     </header>
   );
 }
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../Context";
+import CartItem from "../components/CartItem";
 
 function Cart() {
+  const { cartItems } = useContext(Context);
+  const cartItemElements = cartItems.map((item) => (
+    <CartItem key={item.id} item={item} />
+  ));
+
   return (
     <main className="cart-page">
       <h1>Check out</h1>
+      {cartItemElements}
     </main>
   );
 }
