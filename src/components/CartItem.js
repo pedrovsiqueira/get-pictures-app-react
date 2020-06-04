5bba4c5950245651e2fa5682dbfb2c2654fd8955
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-06-04 10:34:53 -0300

    Add onClick to remove icon

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useContext} from "react";
+import {Context} from '../Context'
 
 const CartItem = ({ item }) => {
+  const { handleRemoveFromCart } = useContext(Context);
+  
   return (
     <div className="cart-item">
-      <i className="ri-delete-bin-line"></i>
+      <i onClick={() => handleRemoveFromCart(item)} className="ri-delete-bin-line"></i>
       <img src={item.url} alt="img" width="130px" />
       <p>$5.99</p>
     </div>
